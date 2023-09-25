ff65cb04427935bed13753d815f2755d51254762
JLisarte <juliano.lisarte@gmail.com>
2023-09-24 21:59:55 -0300

    Atualização da posiçao da paleta de cores

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,3 @@
-/*Add*/ /*
-// Altera a cor de fundo da página
-function changeBackgroundColor(color) {
-    document.body.style.backgroundColor = color;
-    localStorage.setItem('backgroundColor', color);
-}
-//Salva a cor de fundo da página
-const savedColor = localStorage.getItem('backgroundColor');
-if (savedColor) {
-    document.body.style.backgroundColor = savedColor;
-}
-// controla se a paleta de cores está visível ou não
-let isPaletteVisible = false;
-
-// Faz com que a paleta de cores apareça e desapareça alternadamente, e a classe 'active' é usada para aplicar estilos específicos quando a paleta está visível.
-function toggleColorPalette() {
-    isPaletteVisible = !isPaletteVisible;
-    const boxColors = document.getElementById('boxColors');
-    boxColors.classList.toggle('active', isPaletteVisible);
-}
-*/
-
 function bgChangeDiv() {
   let bgChangeDiv = document.getElementById("bgChangeDiv")
   if (bgChangeDiv.style.display === "none") {
