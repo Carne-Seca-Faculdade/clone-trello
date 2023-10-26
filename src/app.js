d6698b271aced846384b5e188b8b6582e4d41c8a
Gabriel Kunst <118089848+gabrielkunst@users.noreply.github.com>
2023-10-25 21:30:46 -0300

    feat - comentários das funções adicionados

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+// GABRIEL
+// FUNÇÃO QUE DEFINE AS VARIÁVEIS E FUNÇÃO PARA SALVAR OS DADOS NO LOCALSTORAGE
 const Store = {
 	backgroundColor: null,
 	columns: null,
@@ -26,6 +28,8 @@ const Store = {
 	},
 };
 
+// LILIAN
+// FUNÇÃO QUE BUSCA OS DADOS NO LOCALSTORAGE
 function getFromLocalStorage(localStorageKey, defaultValue) {
 	try {
 		console.log(`Fetching ${localStorageKey} from localStorage`);
@@ -48,6 +52,8 @@ function getFromLocalStorage(localStorageKey, defaultValue) {
 	}
 }
 
+// LILIAN
+// FUNÇÃO QUE SALVA OS DADOS NO LOCALSTORAGE
 function updateLocalStorage(localStorageKey, data) {
 	try {
 		console.log(`Setting ${localStorageKey} key as `, data);
@@ -59,6 +65,8 @@ function updateLocalStorage(localStorageKey, data) {
 	}
 }
 
+// JULIANO
+// FUNÇÃO QUE RENDERIZA A COR DE FUNDO
 function renderBackgroundColor() {
 	const colorPickerInput = document.querySelector("#colorPickerInput");
 	const backgroundColor = Store.backgroundColor;
@@ -66,12 +74,25 @@ function renderBackgroundColor() {
 	document.body.style.backgroundColor = backgroundColor;
 }
 
+// JULIANO
+// FUNÇÃO QUE SALVA A COR DE FUNDO
 function handleColorInputValueChange(event) {
 	const inputColorValue = event.target.value;
 	console.log("The color picked is ", inputColorValue);
 	Store.saveBackgroundColor(inputColorValue);
 }
 
+// GABRIEL
+// FUNÇÃO QUE MOSTRA O FORMULÁRIO PARA CRIAR UMA NOVA COLUNA
+function handleCreateColumnToggle() {
+	const createColumnForm = document.querySelector(".create-column");
+	const createColumnButton = document.querySelector(".create-column-button");
+	createColumnForm.classList.toggle("hidden");
+	createColumnButton.classList.toggle("hidden");
+}
+
+// GABRIEL
+// FUNÇÃO QUE CRIA UMA NOVA COLUNA
 function handleAddColumnButtonClick() {
 	console.log("Add column button was clicked");
 	const createColumnInput = document.querySelector(".create-column__input");
@@ -97,6 +118,8 @@ function handleAddColumnButtonClick() {
 	handleCreateColumnToggle();
 }
 
+// GABRIEL
+// FUNÇÃO QUE CANCELA A CRIAÇÃO DE UMA NOVA COLUNA
 function handleCancelColumnButtonClick() {
 	const createColumnInput = document.querySelector(".create-column__input");
 	console.log("Column creation was canceled");
@@ -104,13 +127,57 @@ function handleCancelColumnButtonClick() {
 	createColumnInput.value = "";
 }
 
-function handleCreateColumnToggle() {
-	const createColumnForm = document.querySelector(".create-column");
-	const createColumnButton = document.querySelector(".create-column-button");
-	createColumnForm.classList.toggle("hidden");
-	createColumnButton.classList.toggle("hidden");
+// GABRIEL
+// FUNÇÃO QUE RENDERIZA AS COLUNAS
+function renderColumns() {
+	const columnsContainer = document.querySelector(".columns");
+	columnsContainer.innerHTML = "";
+
+	const columns = Store.columns;
+	if (columns.length === 0) {
+		handleCreateColumnToggle();
+	}
+
+	if (columns && columns.length > 0) {
+		const columnsHTML = columns.map(getColumnHTML).join("");
+		columnsContainer.innerHTML = columnsHTML;
+	}
 }
 
+// GABRIEL
+// FUNÇÃO QUE GERA O HTML DE UMA COLUNA
+function getColumnHTML(column) {
+	return `
+		<div class="column" id="column-${
+			column.id
+		}" dropzone="copy" ondrop="handleTaskDropByDrag(event)" ondragover="allowTaskDrop(event)">
+			<div class="column__header">
+				<span class="column__title">${column.title}</span>	
+				<div class="column__options">
+					<button onclick="editColumnTitleById(${column.id})">
+						<i class="fa-solid fa-pencil"></i>
+					</button>
+					<button onclick="deleteColumnById(${column.id})">
+						<i class="fa-regular fa-trash-can"></i>
+					</button>
+				</div>
+			</div>
+			<section class="column__tasks" id="tasks-${column.id}">${column.tasks
+		.map((task) => getColumnTaskHTML(task, column.id))
+		.join("")}</section>
+			<button class="column__create-button"
+			id="create-task-button-${column.id}" onclick="handleCreateTaskButtonClick(${
+		column.id
+	})" >
+				<i class="fa-solid fa-plus"></i>
+				<span>Adicionar tarefa</span>
+			</button>
+		</div>
+	`;
+}
+
+// JULIANO
+// FUNÇÃO QUE DELETA UMA COLUNA
 function deleteColumnById(columnId) {
 	if (!confirm("Tem certeza que deseja excluir a coluna?")) {
 		return;
@@ -124,6 +191,8 @@ function deleteColumnById(columnId) {
 	Store.saveColumns(columnsWithoutDeleted);
 }
 
+// JULIANO
+// FUNÇÃO QUE EDITA O TÍTULO DE UMA COLUNA
 function editColumnTitleById(columnId) {
 	console.log("Editing column with id", columnId);
 	const newColumnTitle = prompt("Digite o novo nome da coluna").trim();
@@ -146,6 +215,8 @@ function editColumnTitleById(columnId) {
 	Store.saveColumns(allColumns);
 }
 
+// EDUARDO
+// FUNÇÃO RESPONSÁVEL RECEBER O EVENTO DE DROP E REALIZAR A MOVIMENTAÇÃO DA TAREFA
 function handleTaskDropByDrag(event) {
 	event.preventDefault();
 	console.log("Task was dropped");
@@ -191,25 +262,119 @@ function handleTaskDropByDrag(event) {
 	}
 }
 
+// EDUARDO
+// FUNÇÃO RESPONSÁVEL POR PERMITIR O DROP DA TAREFA
 function allowTaskDrop(event) {
 	event.preventDefault();
 }
 
-function renderColumns() {
-	const columnsContainer = document.querySelector(".columns");
-	columnsContainer.innerHTML = "";
-
-	const columns = Store.columns;
-	if (columns.length === 0) {
-		handleCreateColumnToggle();
+// EDUARDO
+// FUNÇÃO RESPONSÁVEL POR PEGAR OS DADOS DA TAREFA E TRANSFORMAR EM JSON
+function handleTaskDrag(event, columnId) {
+	const taskTitleElement = event.target.querySelector(".task__title");
+	if (!taskTitleElement) {
+		console.error("No task content found");
+		return;
 	}
 
-	if (columns && columns.length > 0) {
-		const columnsHTML = columns.map(getColumnHTML).join("");
-		columnsContainer.innerHTML = columnsHTML;
-	}
+	const task = {
+		fromColumn: columnId,
+		id: event.target.id,
+		content: taskTitleElement.textContent,
+	};
+
+	const taskAsJson = JSON.stringify(task);
+	event.dataTransfer.setData("application/json", taskAsJson);
 }
 
+// EDUARDO
+// FUNÇÃO RESPONSÁVEL POR PEGAR O ID DA COLUNA QUE A TAREFA ESTÁ
+function getColumnTasksContainer(event) {
+	let target = event.target;
+	let parentNode = event.target.parentNode;
+
+	if (target.id.includes("column")) {
+		const columnId = target.id.split("-")[1];
+		return columnId;
+	}
+
+	while (!parentNode.id.includes("column")) {
+		parentNode = parentNode.parentNode;
+	}
+
+	const columnId = parentNode.id.split("-")[1];
+	return columnId;
+}
+
+// NICOLAS
+// FUNÇÃO QUE GERA O HTML DO FORMULÁRIO DE CRIAÇÃO DE TAREFA
+function getCreateTaskFormHTML(columnId) {
+	return `
+	<div class="create-task-form" id="create-task-form-${columnId}">
+		<input
+			id="create-task-form-input"
+			placeholder="Conteúdo da tarefa..."
+			class="create-task-form__input"
+		/>
+		<div class="create-task-form__buttons">
+			<button class="create-task-form__add" onclick="createTask(${columnId})">Adicionar tarefa</button>
+			<button class="create-task-form__cancel" onclick="handleCancelTaskButtonClick(${columnId})">
+				<i class="fa-solid fa-xmark"></i>
+			</button>
+		</div>
+	</div>
+	`;
+}
+
+// NICOLAS
+// FUNÇÃO QUE MOSTRA O FORMULÁRIO DE CRIAÇÃO DE TAREFA
+function handleCreateTaskButtonClick(columnId) {
+	console.log("Create task button was clicked");
+	console.log(columnId);
+	const columnContainer = document.querySelector(`#column-${columnId}`);
+	console.log(columnContainer);
+	const createTaskButton = document.querySelector(
+		`#create-task-button-${columnId}`
+	);
+	createTaskButton.classList.toggle("hidden");
+	const createTaskFormHTML = getCreateTaskFormHTML(columnId);
+
+	columnContainer.innerHTML += createTaskFormHTML;
+}
+
+// NICOLAS
+// FUNÇÃO QUE CANCELA A CRIAÇÃO DE UMA TAREFA
+function handleCancelTaskButtonClick(columnId) {
+	const createTaskForm = document.querySelector(".create-task-form");
+	const createTaskButton = document.querySelector(
+		`#create-task-button-${columnId}`
+	);
+	console.log("Task creation was canceled");
+	createTaskForm.remove();
+	createTaskButton.classList.toggle("hidden");
+}
+
+// NICOLAS
+// FUNÇÃO QUE GERA O HTML DE UMA TAREFA
+function getColumnTaskHTML(task, columnId) {
+	console.log("the task received is ", task);
+	return `
+		<article class="task" id="${task.id}" draggable="true" ondragstart="handleTaskDrag(event, ${columnId})">
+			<span class="task__title">${task.content}</span>
+			<div class="task__options">
+				<button onclick="editTaskContentById(${task.id},${columnId})">
+					<i class="fa-solid fa-pencil"></i>
+				</button>
+				<button onclick="deleteTaskById(${task.id},${columnId})">
+					<i class="fa-regular fa-trash-can"></i>
+				</button>
+			</div>
+		</article>
+	`;
+}
+
+// NICOLAS
+// FUNÇÃO QUE CRIA UMA NOVA TAREFA
 function createTask(columnId) {
 	console.log("create task ", columnId);
 	const createTaskInput = document.querySelector(".create-task-form__input");
@@ -239,6 +404,8 @@ function createTask(columnId) {
 	Store.saveColumns(allColumns);
 }
 
+// LILIAN
+// FUNÇÃO QUE EDITA O CONTEÚDO DE UMA TAREFA
 function editTaskContentById(taskId, columnId) {
 	const allColumns = Store.columns;
 	const columnToEditTask = allColumns.find((column) => {
@@ -272,6 +439,8 @@ function editTaskContentById(taskId, columnId) {
 	Store.saveColumns(allColumns);
 }
 
+// LILIAN
+// FUNÇÃO QUE DELETA UMA TAREFA
 function deleteTaskById(taskId, columnId) {
 	if (!confirm("Tem certeza que deseja excluir a tarefa?")) {
 		return;
@@ -297,129 +466,8 @@ function deleteTaskById(taskId, columnId) {
 	Store.saveColumns(allColumns);
 }
 
-function handleCreateTaskButtonClick(columnId) {
-	console.log("Create task button was clicked");
-	console.log(columnId);
-	const columnContainer = document.querySelector(`#column-${columnId}`);
-	console.log(columnContainer);
-	const createTaskButton = document.querySelector(
-		`#create-task-button-${columnId}`
-	);
-	createTaskButton.classList.toggle("hidden");
-	const createTaskFormHTML = getCreateTaskFormHTML(columnId);
-
-	columnContainer.innerHTML += createTaskFormHTML;
-}
-
-function handleCancelTaskButtonClick(columnId) {
-	const createTaskForm = document.querySelector(".create-task-form");
-	const createTaskButton = document.querySelector(
-		`#create-task-button-${columnId}`
-	);
-	console.log("Task creation was canceled");
-	createTaskForm.remove();
-	createTaskButton.classList.toggle("hidden");
-}
-
-function handleTaskDrag(event, columnId) {
-	const taskTitleElement = event.target.querySelector(".task__title");
-	if (!taskTitleElement) {
-		console.error("No task content found");
-		return;
-	}
-
-	const task = {
-		fromColumn: columnId,
-		id: event.target.id,
-		content: taskTitleElement.textContent,
-	};
-
-	const taskAsJson = JSON.stringify(task);
-	event.dataTransfer.setData("application/json", taskAsJson);
-}
-
-function getColumnHTML(column) {
-	return `
-		<div class="column" id="column-${
-			column.id
-		}" dropzone="copy" ondrop="handleTaskDropByDrag(event)" ondragover="allowTaskDrop(event)">
-			<div class="column__header">
-				<span class="column__title">${column.title}</span>	
-				<div class="column__options">
-					<button onclick="editColumnTitleById(${column.id})">
-						<i class="fa-solid fa-pencil"></i>
-					</button>
-					<button onclick="deleteColumnById(${column.id})">
-						<i class="fa-regular fa-trash-can"></i>
-					</button>
-				</div>
-			</div>
-			<section class="column__tasks" id="tasks-${column.id}">${column.tasks
-		.map((task) => getColumnTaskHTML(task, column.id))
-		.join("")}</section>
-			<button class="column__create-button"
-			id="create-task-button-${column.id}" onclick="handleCreateTaskButtonClick(${
-		column.id
-	})" >
-				<i class="fa-solid fa-plus"></i>
-				<span>Adicionar tarefa</span>
-			</button>
-		</div>
-	`;
-}
-
-function getColumnTaskHTML(task, columnId) {
-	console.log("the task received is ", task);
-	return `
-		<article class="task" id="${task.id}" draggable="true" ondragstart="handleTaskDrag(event, ${columnId})">
-			<span class="task__title">${task.content}</span>
-			<div class="task__options">
-				<button onclick="editTaskContentById(${task.id},${columnId})">
-					<i class="fa-solid fa-pencil"></i>
-				</button>
-				<button onclick="deleteTaskById(${task.id},${columnId})">
-					<i class="fa-regular fa-trash-can"></i>
-				</button>
-			</div>
-		</article>
-	`;
-}
-
-function getCreateTaskFormHTML(columnId) {
-	return `
-	<div class="create-task-form" id="create-task-form-${columnId}">
-		<input
-			id="create-task-form-input"
-			placeholder="Conteúdo da tarefa..."
-			class="create-task-form__input"
-		/>
-		<div class="create-task-form__buttons">
-			<button class="create-task-form__add" onclick="createTask(${columnId})">Adicionar tarefa</button>
-			<button class="create-task-form__cancel" onclick="handleCancelTaskButtonClick(${columnId})">
-				<i class="fa-solid fa-xmark"></i>
-			</button>
-		</div>
-	</div>
-	`;
-}
-
-function getColumnTasksContainer(event) {
-	let target = event.target;
-	let parentNode = event.target.parentNode;
-
-	if (target.id.includes("column")) {
-		const columnId = target.id.split("-")[1];
-		return columnId;
-	}
-
-	while (!parentNode.id.includes("column")) {
-		parentNode = parentNode.parentNode;
-	}
-
-	const columnId = parentNode.id.split("-")[1];
-	return columnId;
-}
-
+// GABRIEL
+// FUNÇÃO QUE INICIA O APP
 window.addEventListener("DOMContentLoaded", () => {
 	Store.init();
 	renderColumns();
